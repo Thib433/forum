c68e3fac4c2af2d9cbba46b4bd46a2ca36ae3e3e
anthonyassy <anthonyassy56@gmail.com>
2023-01-17 11:10:58 +0100

    accueil
    page d'accueil du forum

--- view/accueilForum.php
+++ view/accueilForum.php
@@ -7,15 +7,15 @@ require('../model/showAllCategorie.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ISH Forum</title>
+    <title>I Connect</title>
     <link rel="stylesheet" href="../assets/accueilForum.css">
 </head>
 <body>
 <div class="titre">
-    ISH Forum
+    I Connect Forum
 </div>
 <div class="texte">
-    <p>Bienvenue sur ISH Forum <br> <br> Ce forum est destiné à tous nos utilisateurs que ce soit médecin ou patient pour discuter, s'entraider et trouver rapidement une solution à un problème.</p>
+    <p>Bienvenue sur I Connect Forum <br> Ce forum est destiné à tous nos utilisateurs que ce soit médecin ou patient pour discuter, s'entraider et trouver rapidement une solution à un problème.</p>
 </div>
 
 <div class="add-cat">
