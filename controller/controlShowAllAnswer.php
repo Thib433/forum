5c81703782c776f841811461cfe7c1e012bd02d8
20doireau <pierre-yves.doireau@mines-paristech.fr>
2023-01-20 00:19:07 +0100

    Commentaires d'Anthony

--- controller/controlShowAllAnswer.php
+++ controller/controlShowAllAnswer.php
@@ -1,13 +1,14 @@
 <?php
-
+//on inclut les fichiers dont on a besoin
 include_once('../core/databaseAction.php');
 require('controlSession.php');
 include_once('../model/showAllAnswerOfQuestionAction.php');
 
       
+
       
-if (isset($_GET['id']) and !empty($_GET['id'])) {
-    $reponse = new GetAnswer($bdd_f);
+if (isset($_GET['id']) and !empty($_GET['id'])) { //On cherche l'id avec la variable $_GET si celle ci n'est pas vide
+    $reponse = new GetAnswer($bdd_f); //On appelle la fonction GetAnswer pr afficher les reponses
     $getAllOfThisQuestion=$reponse ->selectByQuestions($_GET['id']);
-        // do something with the $questions array
+        
 }
--- controller/controlShowArticle.php
+++ controller/controlShowArticle.php
@@ -1,14 +1,15 @@
 <?php
-
+//on inclut les fichiers dont on a besoin
 include_once('../core/databaseAction.php');
 include_once('../model/showArticleContentAction.php');
 
-if (isset($_GET['id']) and !empty($_GET['id'])) {
-    $essai = new ShowA($bdd_f, $_GET['id']);
+
+if (isset($_GET['id']) and !empty($_GET['id'])) { //On cherche l'id avec la variable $_GET si celle ci n'est pas vide
+    $essai = new ShowA($bdd_f, $_GET['id']);  //On appelle la fonction ShowA pour afficher les question ds la page article
     $checkIfQuestionExists = $essai->SelectQuestion();
 
     
-}else{
+}else{ //s'il n y a pas de question message d'erreur
 
     $errorMsg="Aucune question n'a été trouvée";
 }
